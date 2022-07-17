640be4d14d08231018692c2cc5fc5701c023c3e6
Luka Bagic <luka.bagic@sintezis.co>
2022-07-17 02:32:28 +0200

    Cleanup vue example files

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <div class="snt-container-s">
-        <p class="snt-fs-5">Yaay</p>
-      </div>
-      <button class="snt-button secondary small">asdf</button>
-      <button class="snt-button secondary outline">asdf</button>
-      <button class="snt-button secondary text large">asdf</button>
-      <div class="snt-card warning">
-        <table class="snt-table secondary">
-          <thead>
-            <tr>
-              <th>a</th>
-              <th>a</th>
-              <th>a</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="num in 3" :key="num">
-              <td>b{{ num }}</td>
-              <td>b</td>
-              <td>b</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink class="snt-button primary" to="/">Home</RouterLink>
+      <RouterLink class="snt-button secondary outline" to="/about"
+        >About</RouterLink
+      >
+    </nav>
   </header>
 
   <RouterView />
