207f968d992f4f06ca0b43ea639305347389381d
Luka Bagic <luka.bagic@sintezis.co>
2022-07-17 02:38:38 +0200

    Setup store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { pinia } from "./stores/base/pinia";
 
 import App from "./App.vue";
 import { router } from "./router/router.js";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
