43e3aff4dc96a317fefa7fe1522e39809ce4830a
lbagic <luka.bagi@gmail.com>
2022-08-11 10:58:10 +0200

    Add better description for mapEntries

--- src/utils/polyfills/object.d.ts
+++ src/utils/polyfills/object.d.ts
@@ -3,7 +3,7 @@ export {}
 declare global {
   interface ObjectConstructor {
     /**
-     * Returns an object created by key-value entries returned from callback.
+     * Returns an object created by key-value entries returned from callback. Shorthand for Object.fromEntries(Object.entries(o).map(fn)).
      */
     mapEntries<T = any, U = [keyof T, T[keyof T]]>(o: T, callbackfn: (value: U, index: number, array: U[]) => U): T;
    }
