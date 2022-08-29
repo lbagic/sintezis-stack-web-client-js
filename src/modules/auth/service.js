d33306b3e2625f466e2b68bdf122d3fe7535912b
Luka Bagic <luka.bagic@sintezis.co>
2022-08-29 22:55:28 +0200

    Add test service

--- src/modules/auth/service.js
+++ src/modules/auth/service.js
@@ -1,88 +1,13 @@
-import {
-  LoginRequest,
-  RegisterRequest,
-  VerifyEmailRequest,
-} from "../../../util/gen/proto/auth/auth_request_pb";
-import { AuthProviderEnum } from "../../../util/gen/proto/commons/enum_pb";
-import { appleAuth } from "../../services/auth-providers/appleAuth";
-import { facebookAuth } from "../../services/auth-providers/facebookAuth";
-import { googleAuth } from "../../services/auth-providers/googleAuth";
-import { twitterAuth } from "../../services/auth-providers/twitterAuth";
-import { Grpc, grpcOptions } from "../../services/calls/Grpc";
+import { Api } from "../../services/calls/Api";
 
 export const authService = {
-  login: ({ email, password }) =>
-    Grpc.snt
-      .login(
-        new LoginRequest().setEmail(email).setPassword(password),
-        grpcOptions({ useToasts: false })
-      )
-      .then((res) => res.toObject()),
-  register: ({ email, password, passwordConfirmation }) =>
-    Grpc.snt
-      .register(
-        new RegisterRequest()
-          .setEmail(email)
-          .setPassword(password)
-          .setPasswordConfirmation(passwordConfirmation),
-        grpcOptions({ useToasts: false })
-      )
-      .then((res) => res.toObject()),
-  verifyEmail: ({ token }) =>
-    Grpc.snt
-      .verifyEmail(
-        new VerifyEmailRequest().setToken(token),
-        grpcOptions({ useToasts: false })
-      )
-      .then((res) => res.toObject()),
-  googleLogin: () =>
-    googleAuth
-      .signIn()
-      .then(({ credential }) =>
-        Grpc.snt.login(
-          new LoginRequest()
-            .setToken(credential.idToken)
-            .setProvider(AuthProviderEnum.GOOGLE),
-          grpcOptions({ useToasts: false })
-        )
-      )
-      .then((res) => res.toObject()),
-  facebookLogin: () =>
-    facebookAuth
-      .signIn()
-      .then(({ credential }) =>
-        Grpc.snt.login(
-          new LoginRequest()
-            .setToken(credential.accessToken)
-            .setProvider(AuthProviderEnum.FACEBOOK),
-          grpcOptions({ useToasts: false })
-        )
-      )
-      .then((res) => res.toObject()),
-  twitterLogin: () =>
-    twitterAuth
-      .signIn()
-      .then(({ credential }) =>
-        Grpc.snt.login(
-          new LoginRequest()
-            .setToken(credential.accessToken)
-            .setTokenSecret(credential.secret)
-            .setProvider(AuthProviderEnum.TWITTER),
-          grpcOptions({ useToasts: false })
-        )
-      )
-      .then((res) => res.toObject()),
-  appleLogin: () =>
-    appleAuth
-      .signIn()
-      .then(({ credential }) =>
-        Grpc.snt.login(
-          new LoginRequest()
-            .setToken(credential.idToken)
-            .setProvider(AuthProviderEnum.APPLE),
-          grpcOptions({ useToasts: false })
-        )
-      )
-      .then((res) => res.toObject()),
-  logout: () => Promise.resolve(),
+  loginByCredentials: ({ email, password }) =>
+    Api.snt.post("/auth/login/by_credentials", { email, password }),
+  registerByCredentials: ({ email, password, firstName, lastName }) =>
+    Api.snt.post("/auth/register/by_credentials", {
+      email,
+      password,
+      firstName,
+      lastName,
+    }),
 };
