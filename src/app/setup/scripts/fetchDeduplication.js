02cede8290db3cd6eaf148d5ffde83c44241abfc
Luka Bagic <luka.bagic@sintezis.co>
2022-11-04 20:44:09 +0100

    Refactor api layer

--- src/app/setup/scripts/fetchDeduplication.js
+++ src/app/setup/scripts/fetchDeduplication.js
@@ -2,12 +2,16 @@ const map = new Map();
 const { fetch } = window;
 
 window.fetch = async (...args) => {
-  const uid = JSON.stringify(args);
-  const saved = map.get(uid);
-  const call = saved ?? fetch(...args);
-  if (!saved) map.set(uid, call);
-  else console.log("⮑ repeated call");
-  const res = await call;
-  map.delete(uid);
-  return res.clone();
+  try {
+    const uid = JSON.stringify(args);
+    const saved = map.get(uid);
+    const call = saved ?? fetch(...args);
+    if (!saved) map.set(uid, call);
+    else console.log("⮑ repeated call");
+    const res = await call;
+    map.delete(uid);
+    return res.clone();
+  } catch (err) {
+    console.warn(err);
+  }
 };
--- src/services/api-grpc/grpc.js
+++ src/services/api-grpc/grpc.js
@@ -3,14 +3,17 @@ import {
   createGrpcWebTransport,
   createPromiseClient,
 } from "@bufbuild/connect-web";
-import { interceptors } from "./interceptors";
+import { grpcInterceptors } from "../api-interceptors/interceptors";
 
 export const grpc = {
   snt: createPromiseClient(
     GatewayController,
     createGrpcWebTransport({
       baseUrl: "https://envoy.tip.elude.tech:443",
-      interceptors: interceptors.map(({ grpc }) => grpc),
+      interceptors: grpcInterceptors,
     })
   ),
 };
+
+// grpc.snt.getLocations();
+// grpc.snt.getUsers();
