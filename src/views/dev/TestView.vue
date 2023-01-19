2f9cc209415d1132538cd69277bb85726c81400d
Luka Bagic <luka.bagic@sintezis.co>
2023-01-19 02:13:57 +0100

    Create basic component examples

--- src/views/dev/TestView.vue
+++ src/views/dev/TestView.vue
@@ -1,32 +1,9 @@
-<script setup lang="ts">
-import { modalController } from "@/components/base/modal/modalController";
-import ModalNext from "@/components/base/modal/BaseModal.vue";
-// import { NModal } from "naive-ui";
-import { reactive } from "vue";
-
-const m = reactive({
-  a: false,
-  b: false,
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <pre>{{ m }}</pre>
-  <button @click="m.a = true">open a</button>
-  <button @click="modalController.a.show">programatic a</button>
-  <RouterLink to="/test?asdf#hash">open via hash</RouterLink>
-  <RouterLink to="?query&asdf">open via query</RouterLink>
-  <!-- <button @click="m.b = true">open b</button> -->
-  <ModalNext
-    v-model:show="m.a"
-    preset="card"
-    hash="#hash"
-    query="query"
-    name="a"
-  >
-    <template #action>asdf</template>
-  </ModalNext>
-  <!-- <NModal v-model:show="m.b" preset="card">B</NModal> -->
+  <div>
+    <p>TestView</p>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
