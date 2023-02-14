dcf8e34f58cda045692a47898331639bee1f2a84
Luka Bagic <luka.bagic@sintezis.co>
2023-02-14 15:39:03 +0100

    Add naiveui theme color generator

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,30 +1,27 @@
-import { css } from "@/utils/css";
+import { css, type CSS } from "@/utils/css";
 import type { GlobalThemeOverrides } from "naive-ui";
+
+type NThemeColors = "primary" | "info" | "success" | "warning" | "error";
+
+export const generateThemeColor = <T extends NThemeColors>(
+  name: T,
+  paletteColor: CSS.PaletteColor
+) => ({
+  [`${name}Color`]: paletteColor.base,
+  [`${name}ColorHover`]: paletteColor["light-2"],
+  [`${name}ColorPressed`]: paletteColor.dark,
+  [`${name}ColorSuppl`]: paletteColor["light-2"],
+});
 
 export const theme: GlobalThemeOverrides = {
   common: {
     baseColor: css.baseColor.background,
     textColorBase: css.baseColor.text,
-    primaryColor: css.color.primary.base,
-    primaryColorHover: css.color.primary["light-2"],
-    primaryColorPressed: css.color.primary.dark,
-    primaryColorSuppl: css.color.primary["light-2"],
-    infoColor: css.color.info.base,
-    infoColorHover: css.color.info["light-2"],
-    infoColorPressed: css.color.info.dark,
-    infoColorSuppl: css.color.info["light-2"],
-    successColor: css.color.success.base,
-    successColorHover: css.color.success["light-2"],
-    successColorPressed: css.color.success.dark,
-    successColorSuppl: css.color.success["light-2"],
-    warningColor: css.color.warning.base,
-    warningColorHover: css.color.warning["light-2"],
-    warningColorPressed: css.color.warning.dark,
-    warningColorSuppl: css.color.warning["light-2"],
-    errorColor: css.color.error.base,
-    errorColorHover: css.color.error["light-2"],
-    errorColorPressed: css.color.error.dark,
-    errorColorSuppl: css.color.error["light-2"],
+    ...generateThemeColor("primary", css.color.primary),
+    ...generateThemeColor("info", css.color.info),
+    ...generateThemeColor("success", css.color.success),
+    ...generateThemeColor("warning", css.color.warning),
+    ...generateThemeColor("error", css.color.error),
   },
   Divider: {
     color: css.color.muted.base,
