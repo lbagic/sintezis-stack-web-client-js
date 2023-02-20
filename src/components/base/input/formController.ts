c0bc93511caa13b7a56056c872926df7ae718381
Luka Bagic <luka.bagic@sintezis.co>
2023-02-20 16:18:53 +0100

    Fix few types and settle on api signature

--- src/components/base/input/formController.ts
+++ src/components/base/input/formController.ts
@@ -5,25 +5,29 @@ import { computed, reactive, ref } from "vue";
 
 type DataType = Record<PropertyKey, any>;
 type FunctionType = (...args: any[]) => Promise<any>;
-type ResolvedType<T, AltT> = T extends Message<any> | DataType
+type ResolvedType<T, TAction> = T extends Message<any> | DataType
   ? PartialMessage<T & Message<any>>
-  : AltT extends DataType
-  ? PartialMessage<AltT & Message<any>>
+  : TAction extends DataType
+  ? PartialMessage<TAction & Message<any>>
   : DataType;
-// type ResolvedType<T, AltT> = T extends Message<any>
-//   ? PartialMessage<T>
-//   : T extends DataType
-//   ? T
-//   : AltT extends DataType
-//   ? Partial<AltT>
-//   : DataType;
 
 export function useForm<
   T,
-  Call extends FunctionType = never,
-  AltT = Call extends never ? never : Parameters<Call>[0],
-  Data extends ResolvedType<T, AltT> = ResolvedType<T, AltT>
->(data: Data, call?: Call) {
+  Action extends FunctionType = never,
+  TAction = Action extends never ? never : Parameters<Action>[0],
+  Data extends ResolvedType<T, TAction> extends never
+    ? any
+    : ResolvedType<T, TAction> = ResolvedType<T, TAction>,
+  SuccessHandler = Action extends never ? never : (response: TAction) => void,
+  ErrorHandler = Action extends never ? never : (error: any) => void
+>(
+  data: Data,
+  config?: {
+    action?: Action;
+    onSuccess?: SuccessHandler;
+    onError?: ErrorHandler;
+  }
+) {
   const fields = keys(data);
   const ctx = reactive({
     data: data,
@@ -62,29 +66,35 @@ export function useForm<
   const isPending = ref(false);
   const isValid = computed(() => values(ctx.error).every(not));
   const isSubmittable = computed(() => isValid.value && !isPending.value);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let successFn = (response: AltT) => {};
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let errorFn = (error: any) => {};
-  const onSuccess = (handler: typeof successFn) => (successFn = handler);
-  const onError = (handler: typeof errorFn) => (errorFn = handler);
 
-  const submit = () =>
-    call?.(ctx.data)
-      .then((res) => {
-        isPending.value = false;
-        successFn(res);
-      })
-      .catch((error) => {
-        isPending.value = false;
-        errorFn(error);
-      });
+  const hooks: { onSuccess?: SuccessHandler; onError?: ErrorHandler } = {};
+  const onSuccess = (handler: SuccessHandler) => (hooks.onSuccess = handler);
+  const onError = (handler: ErrorHandler) => (hooks.onError = handler);
+
+  const submit = (async () => {
+    const action = config?.action;
+    if (!action) return;
+    isPending.value = true;
+    try {
+      const response = await action(ctx.data);
+      isPending.value = false;
+      if (typeof hooks.onSuccess === "function") hooks.onSuccess(response);
+      if (typeof config.onSuccess === "function") config.onSuccess(response);
+      return response;
+    } catch (error) {
+      isPending.value = false;
+      if (typeof hooks.onError === "function") hooks.onError(error);
+      if (typeof config.onError === "function") config.onError(error);
+      throw error;
+    }
+  }) as () => ReturnType<Action>;
+
   return reactive({
     ...ctx,
     model,
     reset,
     hydrate,
-    submit: submit as Call,
+    submit,
     onSuccess,
     onError,
     isPending,
@@ -92,24 +102,3 @@ export function useForm<
     isSubmittable,
   });
 }
-
-// type TestType = { id: number; name?: string };
-// const fn = (req: TestType) => Promise.resolve(req);
-// useForm();
-
-// useForm<TestType>({ id: 1 });
-// useForm<TestType>({});
-
-// useForm<User>({ id: 1 });
-// useForm<User>({});
-
-// useForm({ id: 1 }, fn);
-// useForm({}, fn);
-
-// async function login(payload: LoginRequest) {
-//   const response = await grpc.AccountService.login(payload);
-//   response;
-// }
-
-// useForm({ provider: 0 }, login);
-// useForm({ address: "asd" }, grpc.UserService.add);
