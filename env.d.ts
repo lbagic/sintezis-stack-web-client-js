a8a0cc93906d15010995f93440516bc0e2bbd807
Luka Bagic <luka.bagic@sintezis.co>
2023-02-20 17:37:09 +0100

    Add google map to env files

--- env.d.ts
+++ env.d.ts
@@ -16,6 +16,9 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+
+  // Google maps
+  readonly VITE_GOOGLE_MAPS_API_KEY: string;
 }
 
 interface ImportMeta {
