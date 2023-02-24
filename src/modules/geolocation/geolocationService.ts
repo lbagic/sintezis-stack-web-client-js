77c415976ca6365aa66181639e5d3b816d10242a
Luka Bagic <luka.bagic@sintezis.co>
2023-02-24 02:24:17 +0100

    Cleanup geolocation service

--- src/modules/geolocation/geolocationService.ts
+++ src/modules/geolocation/geolocationService.ts
@@ -1,17 +1,23 @@
+const defaultOptions: PositionOptions = {
+  maximumAge: 1000 * 60 * 60,
+  timeout: 10000,
+};
+
+function getCurrentPosition(options?: PositionOptions) {
+  return new Promise<GeolocationPosition>((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      resolve,
+      reject,
+      options ?? defaultOptions
+    );
+  });
+}
+
+function getPermissionStatus() {
+  return navigator.permissions.query({ name: "geolocation" });
+}
+
 export const geolocationService = {
-  getCurrentPosition(options?: PositionOptions) {
-    return new Promise<GeolocationPosition>((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(
-        resolve,
-        reject,
-        options ?? {
-          maximumAge: 1000 * 60 * 60,
-          timeout: 7000,
-        }
-      );
-    });
-  },
-  getPermissionStatus() {
-    return navigator.permissions.query({ name: "geolocation" });
-  },
+  getCurrentPosition,
+  getPermissionStatus,
 };
