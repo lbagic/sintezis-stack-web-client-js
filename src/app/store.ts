9f351cc10597944620259185a68ed5daaa7f5f54
Luka Bagic <luka.bagic@sintezis.co>
2023-05-07 06:43:58 +0200

    Cleanup store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,5 @@
 import { router } from "@/app/router";
+import type { PiniaPluginContext } from "pinia";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { markRaw } from "vue";
@@ -6,30 +7,29 @@ import type { Router } from "vue-router";
 
 export const pinia = createPinia();
 
-// Plugin - persisted state
-pinia.use(piniaPluginPersistedstate);
+function piniaPluginResetstate({ store, options }: PiniaPluginContext) {
+  type State = (typeof store)["$state"];
+  type Reset = Record<keyof State, () => void>;
 
-// Plugin - rotuer
+  const reset: Reset = {};
+  const initialState = options.state?.();
+  Object.keys(store.$state).forEach((key) => {
+    reset[key] = () => (store.$state[key] = initialState?.[key]);
+  });
+  return { reset };
+}
+
+pinia.use(piniaPluginPersistedstate);
+pinia.use(piniaPluginResetstate);
 pinia.use(({ store }) => {
   store.router = markRaw(router);
 });
 
-// Plugin - reset
-pinia.use(({ store, options }) => {
-  const reset = Object.keys(store.$state).reduce((o, key) => {
-    // o[key] = () => store.$patch({ [key]: options.state?.()[key] });
-    o[key] = () => (store.$state[key] = options.state?.()[key]);
-    return o;
-  }, {} as Record<keyof typeof store.$state, () => void>);
-  if (import.meta.env.DEV) store._customProperties.add("reset");
-  return { reset };
-});
-
-// Type definitions for store
 declare module "pinia" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export interface PiniaCustomProperties<Id, S, G, A> {
-    reset: { [K in keyof S]: () => void };
+    // @ts-ignore
+    reset: { [K in keyof this["$state"]]: () => void };
     router: Router;
   }
 }
